290a79cd1ed00b2dd292d035817eb73610ac8285
Sethu-Natarajan <127437830+Sethu-Natarajan@users.noreply.github.com>
2023-03-30 10:17:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,16 @@
-
+def generate_matchups(n_array):  
+  
+  if len(n_array) > 1:
+  
+    median_id = len(n_array) //2
+    left_array = n_array[:median_id]
+    right_array = n_array[median_id:]
+    right_array.reverse()
+    
+    buffer_array = [i for i in zip(left_array, right_array)]    
+    processed_array = generate_matchups(buffer_array)
+    
+    return processed_array
+    
+   else:
+     return n_array 
